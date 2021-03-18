791adf1f6884b7ab9c55b5c91349b423d33983fd
DwieStronyMocy <igomidormid@gmail.com>
2021-03-18 13:06:23 +0100

    Ver 1.0

--- Models/Address.cs
+++ Models/Address.cs
@@ -9,6 +9,8 @@ namespace zajęcia_3.Models
     public class Address
     {
         [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage ="Pole jest obowiązkowe")]
+
         public string Street { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
